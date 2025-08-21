f2cb17e9298e58c609492e72cd5cec8c15b2a1d9
sofia1979 <sophiedubinska@gmail.com>
2025-08-21 14:00:03 +0300

    автоклікер

--- практичні/практична11.py
+++ практичні/практична11.py
@@ -1,8 +1,53 @@
 from tkinter import *
+from tkinter import messagebox
+import time
+import keyboard
+import mouse
+
+
+
+running = False # змінна, що зберігатиме стан: програма зараз працює або ні
+delay = 0 # змінна, що зберігатиме тривалість перерви після кожного кліку
+
+def exit_app():
+
+    global running
+    running = False
+    messagebox.showinfo("Auto Clicker", "Auto Clicker зупинено.")
+    homik.destroy() # Закриття вікна Tkinter
+
+
+def start_clicker():
+    global running, delay # "знаходимо" змінні, що існують поза функцією
+    clicks_per_second = int(user.get())
+    delay = 1 / clicks_per_second # рахуємо затримку між кліками
+    messagebox.showinfo("Auto Clicker", "Auto Clicker розпочинає роботу.")
+    running = True
+
+    # Запуск процесу кліків
+    schedule_click()
+
+
+def schedule_click():
+    if running:
+        mouse.click()
+        time.sleep(delay) # затримка між кліками
+        schedule_click() # функція знову викликає сама себе
+
+
+def show_info(event):
+    messagebox.showinfo("Інформація", "Це автоклікер, він буде клікати мишкою зі швидкістю, яку ти вкажеш!")
 
 homik = Tk()
 homik.title('Auto Clicker')
-homik.geometry('300x300')
+
+screenWidth = homik.winfo_screenwidth()
+screenHeight = homik.winfo_screenheight()
+
+newWidth = screenWidth // 2
+newHeight = screenHeight // 2
+
+homik.geometry(f"{newWidth}x{newHeight}")
 homik.config(bg='lightblue')
 
 l1 = Label(homik, text='Auto Clicker', fg='#4F7972', font=('Arial', 20), bg='lightblue')
@@ -11,14 +56,25 @@ l1.pack(side='top', pady=10)
 l2 = Label(homik, text='Кліків на секунду', fg='#4F7972', font=('Arial', 15), bg='lightblue')
 l2.pack(side='top', pady=15)
 
+l3 = Label(homik, text='Інформація:\n Це автоклікер, він буде клікати мишкою зі швидкістю, яку ти вкажеш!\n Ти можеш вийти задопомогою кнопки ESC на клавіатурі', bg='grey', fg='white', font=('Arial',14))
+l3.pack(side='top')
 
-inputs = Entry(homik)
-inputs.pack(side='top', pady=20)
+user = Entry(homik, width=30, font=('Arial', 15))
+user.pack(side='top', pady=30)
 
-b1 = Button(homik, text='Почати', fg='white', bg='#5CBE31', font=('Arial', 14))
-b1.pack(side='left', padx=20)
+frame_b1 = Frame(homik, bg='darkgreen')
+frame_b1.pack(side='left', padx=20, pady=20, expand=True)
 
-b2 = Button(homik, text='Вийти', fg='white', bg='#E13232', font=('Arial', 14), command=homik.destroy)
-b2.pack(side='right', padx=50)
+frame_b2 = Frame(homik, bg='darkred')
+frame_b2.pack(side='left', padx=20, pady=20, expand=True)
+
+
+b1 = Button(frame_b1, text='Почати', fg='white', bg='#5CBE31', font=('Arial', 24), command=start_clicker)
+b1.pack()
+
+b2 = Button(frame_b2, text='Вийти', fg='white', bg='#E13232', font=('Arial', 24), command=homik.destroy)
+b2.pack()
+
+keyboard.add_hotkey('esc', exit_app)
 
 homik.mainloop()
